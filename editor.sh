e7c4fdd68c962e5ccf01222531792b9e28919514
Igor <igor.franic@knowit.tech>
2022-04-22 08:44:50 +0200

    download neovim

--- editor.sh
+++ editor.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo 'editor'
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage ~/bin/nvim/nvim.appimage
+
+# update-alternatives
+# ./nvim.appimage
 
 
